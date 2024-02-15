7df0f119796832188c1ea5b0a5c03b4736c4c6a6
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-02-15 15:19:35 -0500

    change port settings

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.put('/image', (req,res) =>{ imageController.handleImage(req,res, db)});
 
 app.post('/clarifai', (req,res) => clarifaiController.handleClarifai(req,res));
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT || 3000}`);
 })
