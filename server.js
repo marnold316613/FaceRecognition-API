db3b98e7f608c81d501ffba40c66ed7e9eb1835c
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-02-21 19:53:49 -0500

    more cors

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
  // res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Origin, X-Requested-With, Content-Type, Accept,content-type,application/json, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.header("Access-Control-Allow-Methods", "POST, GET, HEAD ,OPTIONS, PUT, DELETE");
   next();
 })
