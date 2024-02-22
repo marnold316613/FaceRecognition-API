5f410a62c6fbc6874bf4adf0f9fccf6b58238b32
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-02-21 19:48:14 -0500

    troubleshooting cors

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.use(express.json());
 //app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,content-type,application/json");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+ // res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Origin, X-Requested-With, Content-Type, Accept,content-type,application/json, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.header("Access-Control-Allow-Methods", "POST, GET, HEAD ,OPTIONS, PUT, DELETE");
   next();
 })
 
