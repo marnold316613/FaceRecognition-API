3b9cac883c074af8b67f7326e387d1788cceeca8
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-02-21 19:37:51 -0500

    added more cors options

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.use(express.json());
 //app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,content-type,application/json");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   next();
 })
